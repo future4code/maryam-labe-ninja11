5d13cd199f7c63ef0bb0343fce677c03e49a4b22
Milena Lara <milenalrf@gmail.com>
2021-09-28 22:42:59 -0300

    Coloquei links pras fontes no index.html, criei Estilos.js com padronizações do projeto, comecei a fazer o CardServicos.js

---
+++ src/components/CardServicos/EstiloCardServicos.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const EstiloCardServicos = styled.div`
+  width: 10%;
+  height: 10%;
+  padding: 20px;
+  background-color: #FFF;
+  border: 2px solid #8265c9;
+
+  h1,h2,h3,h4,h5,h6,p{
+    padding: 0;
+    margin: 0;
+  }
+`
