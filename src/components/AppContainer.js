8ac4ed99f40a08f6306a1351492c4ea58a075d82
Milena Lara <milenalrf@gmail.com>
2021-09-30 13:54:39 -0300

    Deletei o CardServicos do AppContainer.js

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import EstilosAplicados from './EstilosAplicados';
-import CardServicos from './CardServicos/CardServicos';
 import Home from './Home/Home';
 import Header from './Header/Header';
 import TelaCarrinho from './TelaCarrinho/TelaCarrinho';
@@ -41,7 +40,6 @@ export class AppContainer extends Component {
     }
   }
 
-
   render() {
     return (
       <div>
@@ -49,14 +47,6 @@ export class AppContainer extends Component {
           trocarTela={this.trocarTela}
         />
         {this.renderizarTela()}
-
-        {/* O componente EstilosAplicados é só para testar os estilos do Estilos.js e pode ser comentado  */}
-        {/* <EstilosAplicados />
-        <CardServicos
-          titulo={'Título'}
-          dataFinal={'06/08/2021'}
-          preco={'800,00'}
-        /> */}
       </div>
     )
   }
