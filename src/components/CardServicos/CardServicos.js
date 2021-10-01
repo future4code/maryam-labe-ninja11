f4616ea5032a638685ecec6954089995d8e887a9
Mariana Carolina de Souza <smcarolina42@gmail.com>
2021-10-01 18:20:21 -0300

    resolvi erro na pagina detalhes

--- src/components/CardServicos/CardServicos.js
+++ src/components/CardServicos/CardServicos.js
@@ -18,17 +18,20 @@ import {
 } from "./EstiloCardServicos";
 import TelaDetalhes from "../TelaDetalhesCardServicos/TelaDetalhes";
 import Botao from "../Botao/Botao";
+import { convertDate } from './ConvertDate';
+
+
 
 export default class CardServicos extends React.Component {
   render() {
-
+  
     return (
       <EstiloCardServicos>
         <Header2>{this.props.titulo}</Header2>
-        <TextoCorpo>Até {this.props.dataFinal} por</TextoCorpo>
+        <TextoCorpo>Até {convertDate(this.props.dataFinal)}</TextoCorpo>
         <TextoCorpoStrong> R${this.props.preco}</TextoCorpoStrong>
         <ContainerDetalhes>
-          <Header4><a href="#" onClick={() => this.props.mostraDetalhe(this.props.produto)}>Ver detalhes</a></Header4>
+          <Header4><a href="#" onClick={() => this.props.mostraDetalhe(this.props.servico)}>Ver detalhes</a></Header4>
           <img src={Carrinho} />
         </ContainerDetalhes>
         <Botao
